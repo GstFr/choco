8f6b00d14532800ede56b095984a884fde664826
Gaston Freire <gastonfreire@outlook.com>
2023-05-09 10:33:55 -0300

    entrega final 1

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,8 +1,21 @@
 import ItemCount from "../ItemCount/ItemCount"
-
-
+import { useContext, useState } from "react"
+import { Link } from "react-router-dom"
 
 const ItemmDetail = ({ id, name, img, category, description, price, stock }) => {
+
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    const { addItem } = useContext(CartContext)
+
+    const handleOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+
+        const item = {
+            id, name, price
+        }
+        addItem(item, quantity)
+    }
     return (
         <div className="text-center">
             <div className="card" >
@@ -14,8 +27,14 @@ const ItemmDetail = ({ id, name, img, category, description, price, stock }) => 
                     <p className="card-text">Precio: ${price}</p>
                     <p className="card-text">Stock disponible:{stock}</p>
                     <footer className="Itemfooter">
-                        <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada', quantity)} />
-                    </footer>
+                    {
+                            quantityAdded > 0 ? (
+                                <Link to='/cart' className='btn btn-primary'>Terminar compra</Link>
+                            ) : (
+                                <ItemCount initial={1} stock={stock} onAdd={handleOnAdd} />
+                            )
+                        }
+                        </footer>
                 </div>
             </div>
         </div>
@@ -23,3 +42,5 @@ const ItemmDetail = ({ id, name, img, category, description, price, stock }) => 
 }
 
 export default ItemmDetail
+
+
