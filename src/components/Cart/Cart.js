af11114c2d7bad92701a959b38019ee9e28223b9
Gaston Freire <gastonfreire@outlook.com>
2023-05-09 11:00:46 -0300

    entrega final 2

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,35 @@
+import './Cart.css'
+import { useContext } from "react"
+import { CartContext } from "../context/CartContext"
+import { Link } from "react-router-dom"
+import CartItem from '../CartItem/CartItem'
+
+
+
+const Cart = () => {
+    const { cart, clearCart, totalQuantity, total } = useContext(CartContext)
+
+
+
+    if (totalQuantity === 0) {
+        return (
+            <div className='SinItems'>
+                <h1>No hay Items en el carrito</h1>
+                <Link to='/' className="btn btn-light" >Ver todos los Productos</Link>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            {cart.map(p => <CartItem key={p.id} {...p} />)}
+            <div className="cartCaja">
+                <h3 className='cartTotal'> Total: ${total}</h3>
+                <button onClick={() => clearCart()} className="btn btn-primary buttonLimpiar">Limpiar carrito</button>
+                < botton type="button" className="btn btn-warning buttonChecout" ><Link to='/checkout' className='chekoutLink' >Checkout Order</Link></botton >
+            </div>
+        </>
+    )
+}
+
+export default Cart
