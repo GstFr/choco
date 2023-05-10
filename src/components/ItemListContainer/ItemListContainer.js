afd79ab1e418633a7cd1730c805ba877c7f46649
Gaston Freire <gastonfreire@outlook.com>
2023-05-10 19:04:52 -0300

    firebase

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -14,7 +14,7 @@ const ItemListContainer = ({ greeting }) => {
     const { categoryId } = useParams()
 
     useEffect(() => {
-        setLoading(true)
+        setLoading(true)entre
 
         const collectionRef = categoryId
             ? query(collection(db, 'products'), where('category', '==', categoryId))
