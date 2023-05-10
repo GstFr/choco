1994aa2389f9edb9600496959e93793519f4fe51
Gaston Freire <gastonfreire@outlook.com>
2023-05-10 20:35:20 -0300

    final

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -8,7 +8,7 @@ const CartWidget = () => {
     const { totalQuantity } = useContext(CartContext)
 
     return (
-        <Link to='/cart' className='carrito' style={{ display: totalQuantity > 0 ? 'block' : 'none' }}>
+        <Link to='/cart' className='cart' style={{ display: totalQuantity > 0 ? 'block' : 'none' }}>
             <img className='cart-img' src={Cart} alt="cart-widget" />
             {totalQuantity}
         </Link>
